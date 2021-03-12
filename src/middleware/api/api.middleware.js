2635b07e014f888b6b30620839abaa0dd12e7193
jobpersonal <jobocanegra@personalsoft.com.co>
2021-03-12 09:21:25 -0500

    updated api.middleware

--- src/middleware/api/api.middleware.js
+++ src/middleware/api/api.middleware.js
@@ -4,14 +4,22 @@ const jwt = require('jsonwebtoken');
 const apiMiddleware = (req, res, next) => {
   const auth_header = req.headers['authorization'];
   const token = auth_header && auth_header.split(' ')[1];
-
-  if (token == null) return res.sendStatus(401);
-
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
-    req.user = user;
-    next();
-  });
+  
+  if (token == null) {
+    res.sendStatus(401);
+    res.end();
+  } else {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+      if (err) {
+        res.sendStatus(403);
+        res.end();
+      } else {
+        req.user = user;
+        next();
+      }
+        
+    });
+  }
 };
 
 exports.apiMiddleware = apiMiddleware;
